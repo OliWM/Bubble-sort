1d9bec575929984a16b772c70ffa49f945e07aea
OliWM <oliver@climaider.com>
2025-10-28 15:04:08 +0100

    working letters and numbers in order, no swap yet

--- src/main.c
+++ src/main.c
@@ -1,34 +1,69 @@
 #include <sorting.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 char letter_arr1[] = {'z', 'S', 's', 'a'};
 // aSsz
 char number_arr1[] = {4, -1, 2, 9};
 // -1, 2, 4, 9
-// overvej at gør så det er et userinput
+// overven at gør så det er et userinput
 
 // bubble_sort(char arr, bool(pair_is_in_order *)(char a, char b)); samler bare
 // de andre funktioner
 
 // pair_is_in_order
 
-/* letters_in_order /brug ASCII values? bare treat som ints. Handle at 65 = 97
-osv. (+32) letters_in_order('a', 'B'), // true letters_in_order('b', 'a'); //
-false letters_in_order('a', 'A'), // true */
+// tjek hvordan du beslutter om du skal bruge letter eller number
 
-/*numbers_ascending
-numbers_in_ascending_order(1, 2); // true
-numbers_in_ascending_order(1, 1); // true
-numbers_in_ascending_order(1, -1); // false*/
+int n = 0;
+
+int convert_lowercaseletters(char letter) {
+  if (letter < 65 || (90 < letter && letter < 97) ||
+      letter > 122) { // in *letter because we take the ASCII values
+    // check om den er i store bogstaver eller små bogstaver range
+    puts("Invalid input, non-letter detected");
+    return -1;
+  }
+  if (97 < letter && letter < 122) {
+    // convert lowercase letter value to equivalent uppercase value for
+    // comparison
+    return letter - 32;
+  } else {
+    return letter;
+  }
+}
+// convert second letter too? but only temporarily
+
+bool letters_in_order(char *letters, int n) {
+  int j = n;
+  int first_letter = convert_lowercaseletters(letters[j]);
+  j++;
+  int second_letter = convert_lowercaseletters(letters[j]);
+
+  bool in_order = first_letter < second_letter ? true : false;
+  return in_order;
+}
+
+bool numbers_in_ascending_order(char *numbers, int n) {
+  bool numbers_in_order = numbers[n] < numbers[n + 1] ? true : false;
+  return numbers_in_order;
+}
 
 /*swap brug pointers til at ændre addresserne
 char arr[] = {1, 2, 3};
 swap(arr[0], arr[2]);
 // Now is arr = {3, 2, 1}*/
 
-int a = 'b';
-
 int main() {
-  printf("%c", a);
+  int n = 0;
+  bool letter_in_order = letters_in_order(letter_arr1, n);
+
+  printf("Is %c before %c? \n%s", letter_arr1[n], letter_arr1[n + 1],
+         letter_in_order ? "True \n" : "False\n");
+
+  bool number_in_order = numbers_in_ascending_order(number_arr1, n);
+
+  printf("Is %d before %d? \n%s", number_arr1[n], number_arr1[n + 1],
+         number_in_order ? "True\n" : "False\n");
   return 0;
 }
